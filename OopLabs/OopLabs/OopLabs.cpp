ed7c83a033886c1ce676660127edd6615b5dc0cf
stlegion277 <sstlegion277@gmail.com>
2018-02-24 20:59:23 +0700

    printfs added instead of cout

--- OopLabs/OopLabs/OopLabs.cpp
+++ OopLabs/OopLabs/OopLabs.cpp
@@ -215,21 +215,21 @@ int main()
 	bool menu = 1;
 	int default = 10000;
 
-	cout << endl << "1. First task" << endl
-		<< "2. Second task" << endl
-		<< "3. Third task" << endl
-		<< "4. Fourth task" << endl
-		<< "5. Fifth task" << endl
-		<< "6. Sixth task" << endl
-		<< "7. Seventh task" << endl
-		<< "8. Eighth task" << endl
-		<< "9. Ninth task" << endl
-		<< "10. Tenth task" << endl
-		<< "11. Eleventh task" << endl
-		<< "12. Twelfth task" << endl
-		<< "13. Thriteenth task" << endl
-		<< "0 Exit " << endl
-		<< "Enter number of function to start ";
+	printf("1. First task\n");
+	printf("2. Second task\n");
+	printf("3. Third task\n");
+	printf("4. Fourth task\n"); 
+	printf("5. Fifth task\n"); 
+	printf("6. Sixth task\n"); 
+	printf("7. Seventh task\n"); 
+	printf("8. Eighth task\n"); 
+	printf("9. Ninth task\n"); 
+	printf("10. Tenth task\n"); 
+	printf("11. Eleventh task\n"); 
+	printf("12. Twelfth task\n"); 
+	printf("13. Thriteenth task\n"); 
+	printf("0 Exit\n "); 
+	printf(	 "Enter number of function to start\n ");
 	m = CheckSymbol();
 
 	while (menu == 1)
@@ -242,91 +242,91 @@ int main()
 		case 1:
 		{
 			helloWorld();
-			cout << "Enter number of function to start ";
+			printf( "Enter number of function to start ");
 			m = CheckSymbol();
 			break;
 		}
 		case 2:
 		{
 			numbersSumm();
-			cout << "Enter number of function to start ";
+			printf("Enter number of function to start ");
 			m = CheckSymbol();
 			break;
 		}
 		case 3:
 		{
 			numbersDivision();
-			cout << "Enter number of function to start ";
+			printf( "Enter number of function to start");
 			m = CheckSymbol();
 			break;
 		}
 		case 4:
 		{
 			readingInput();
-			cout << "Enter number of function to start ";
+			printf( "Enter number of function to start");
 			m = CheckSymbol();
 			break;
 		}
 		case 5:
 		{
 			pointerExample();
-			cout << "Enter number of function to start ";
+			printf( "Enter number of function to start");
 			m = CheckSymbol();
 			break;
 		}
 		case 6:
 		{
 			exampleIf();
-			cout << "Enter number of function to start ";
+			printf("Enter number of function to start ");
 			m = CheckSymbol();
 			break;
 		}
 		case 7:
 		{
 			exampleElseIf();
-			cout << "Enter number of function to start ";
+			printf( "Enter number of function to start");
 			m = CheckSymbol();
 			break;
 		}
 		case 8:
 		{
 			exampleElseIf();
-			cout << "Enter number of function to start ";
+			printf( "Enter number of function to start ");
 			m = CheckSymbol();
 			break;
 		}
 		case 9:
 		{
 			predfixCycle();
-			cout << "Enter number of function to start ";
+			printf("Enter number of function to start ");
 			m = CheckSymbol();
 			break;
 		}
 		case 10:
 		{
 			postfixCycle();
-			cout << "Enter number of function to start ";
+			printf("Enter number of function to start ");
 			m = CheckSymbol();
 			break;
 		}
 		case 11:
 		{
 			iterativeCycle();
-			cout << "Enter number of function to start ";
+			printf("Enter number of function to start ");
 			m = CheckSymbol();
 			break;
 		}
 		case 12:
 		{
 			convertingTypes();
-			cout << "Enter number of function to start ";
+			printf( "Enter number of function to start ");
 			m = CheckSymbol();
 			break;
 		}
 		case 13:
 		{
 			breakExample();
-			cout << "Enter number of function to start ";
+			printf("Enter number of function to start ");
 			m = CheckSymbol();
 			break;
 		}
