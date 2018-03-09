fd29904f2e51644708c2c52fe3901ae7106e0d74
stlegion277 <sstlegion277@gmail.com>
2018-03-10 01:21:25 +0700

    Version 0.1

---
+++ OOPLabs2/OOPLabs2/Functions.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "CheckSymbol.h"
+
+
+namespace KEK
+{
+	
+
+void PrintHelloWorld();
+void GlobalPlusTwo();
+void GlobalMultiplyThree();
+void GlobalEqualsOne();
+double MakeCalculation(int value1, int value2);
+int Divide(int dividend, int divisor);
+int GetRoots(int a, int b, int c, double* x1, double* x2);
+int GetRoots2(int a, int b, int c, double& x3, double& x4);
+int GetPower(int base, int power);
+void BubbleSort(int intArray[5]);
+int** MultiplyMatrix(int sizeArr);
+}
