da8ab2d6b39069c18219b8aca6a02383871944f5
stlegion277 <sstlegion277@gmail.com>
2018-03-29 23:48:10 +0700

    new version of lab with working Windows.h file

---
+++ ObjectOrientedProgrammingLabs3/ObjectOrientedProgrammingLabs/Person.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "CheckSymbol.h"
+#include "Lab3.h"
+#include "Sex.h"
+
+namespace Lab3
+{
+	//TODO: Ќеправильное форматирование!
+	//исправил
+	struct Person 
+	{
+		char Surname[40];
+		char Name[20];
+		Sex Sex;
+	};
+}
---
+++ ObjectOrientedProgrammingLabs3/ObjectOrientedProgrammingLabs/Sex.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "CheckSymbol.h"
+//TODO: Ќеправильное форматирование!
+//исправил
+enum Sex 
+{
+	Female, 
+	Male 
+};
